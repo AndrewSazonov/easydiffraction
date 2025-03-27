c9a3e1433384e015f7e0ae25beb5d62d27472f07
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-03-27 22:35:08 +0100

    Enhance Descriptor class with new attributes

--- src/easydiffraction/core/parameter.py
+++ src/easydiffraction/core/parameter.py
@@ -1,10 +1,21 @@
+from easydiffraction.utils.formatting import warning
+
+
 class Descriptor:
     """
     Base class for descriptors (non-refinable attributes).
     """
 
-    def __init__(self, value, cif_name, block_name=None, units=None):
+    def __init__(self,
+                 value,
+                 cif_name,
+                 block_name=None,
+                 units=None,
+                 description=None,
+                 editable=True):
         self._value = value
+        self._description = description
+        self._editable = editable
         self.cif_name = cif_name
         self.units = units
         self.is_parameter = False  # Differentiates from Parameter class
@@ -29,28 +40,51 @@ class Descriptor:
     def __set__(self, instance, value):
         self._value = value
 
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, new_value):
-        self._value = new_value
-
     def __repr__(self):
         return f"{self._value}"
 
     def __str__(self):
         return str(self._value)
 
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if self._editable:
+            self._value = new_value
+        else:
+            print(warning(f"The parameter '{self.cif_name}' it is calculated automatically and cannot be changed manually."))
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def editable(self):
+        return self._editable
 
 class Parameter(Descriptor):
     """
     A parameter with a value, uncertainty, units, and CIF representation.
     """
 
-    def __init__(self, value, cif_name, block_name=None, uncertainty=0.0, free=False, units=None, min_value=None, max_value=None):
-        super().__init__(value, cif_name, block_name, units)
+    def __init__(self,
+                 value,
+                 cif_name,
+                 block_name=None,
+                 description=None,
+                 uncertainty=0.0,
+                 free=False,
+                 units=None,
+                 min_value=None,
+                 max_value=None):
+        super().__init__(value,
+                         cif_name,
+                         block_name,
+                         units,
+                         description)
         self.uncertainty = uncertainty
         self.free = free
         self.is_parameter = True  # Flag to detect parameters
